5e84eecfd4b7ad42f89c7b02cf11b771bbd0eec8
SMHRD <s20194441@gmail.com>
2024-06-11 16:45:52 +0900

    서브 기능 수정

--- lib/chatbot/api_service.dart
+++ lib/chatbot/api_service.dart
@@ -1,10 +1,21 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:propetsor/main.dart';
+import 'package:provider/provider.dart';
 
 class APIService {
   Future<String> sendMessage(String message, String breed, String age) async {
+    String? uidx = await storage.read(key:"uidx");
+    String u_idx;
+
+    if (uidx != null){
+      u_idx = uidx;
+    }else{
+      u_idx = "1";
+    }
+
     final response = await http.post(
-      Uri.parse('http://211.48.213.165:5000/chat'),
+      Uri.parse('http://211.48.213.165:5001/chat'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -12,6 +23,7 @@ class APIService {
         'query': message,
         'breed': breed,
         'age': age,
+        'uidx': u_idx,
       }),
     );
 
